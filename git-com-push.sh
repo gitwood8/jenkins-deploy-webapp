00dd6d9212d462adbb3967b04dceddacd88080f7
Aleksei Volkov <volkov.a.n.biz@gmail.com>
2023-05-31 07:58:58 +0000

    test scr

--- git-com-push.sh
+++ git-com-push.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 read -p "Enter a comment for commit: " COM
-git add .
-git commit -m "$COM"
-git push
+read -p "Ready to Commit and Push with comment: '$COM'? y/n: " ANS
+if [ $ANS == "y" ] 
+then
+    git add .
+    git commit -m "$COM"
+    git push
+#    echo "Nice"
+    exit 0
+else
+    echo "Aborted"
+    exit 1
+fi
