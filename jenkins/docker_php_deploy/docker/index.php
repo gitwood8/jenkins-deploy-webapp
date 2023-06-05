9987fdab049a59710360c14dd18e40c6517dc10f
Aleksei Volkov <volkov.a.n.biz@gmail.com>
2023-06-05 06:04:13 +0000

    edit 3.22 index

--- jenkins/docker_php_deploy/docker/index.php
+++ jenkins/docker_php_deploy/docker/index.php
@@ -1,7 +1,9 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
+echo "<div style='text-align: center;'>";
 echo "<h1>Hello from K8s</h1><br>";
-echo "Server ip is: $ip_server", "<br><p>";
-echo "Made by <font color=grey>Wood", "<br><p>";
+echo "Server IP is: $ip_server", "<br><p>";
+echo "Made by <font color=grey>Wood</font></p><br>";
 echo "<h1><font color=blue>VERSION latest</h1>";
+echo "</div>";
 ?>
