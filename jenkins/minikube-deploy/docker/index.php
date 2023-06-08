11ffb969d219cc26e6f9e2c6e326005b984435d2
Aleksei Volkov <volkov.a.n.biz@gmail.com>
2023-06-08 11:02:50 +0000

    edit 6.0 new branch minikube

---
+++ jenkins/minikube-deploy/docker/index.php
@@ -0,0 +1,54 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.container {
+  background-color: #f2f2f2;
+  border: 1px solid #ccc;
+  padding: 30px;
+  margin: 0 auto;
+}
+
+h1 {
+  text-align: center;
+  animation: moveText 2s infinite alternate;
+}
+
+@keyframes moveText {
+  0% {
+    margin-left: 0;
+  }
+  50% {
+    margin-left: 30px;
+  }
+  100% {
+    margin-left: 0;
+  }
+}
+</style>
+</head>
+<body>
+<div class="row">
+  <div class="container">
+    <h1>Hello from Docker Container</h1>
+    <br>
+    <p>Server IP is: <?php echo $ip_server; ?></p>
+    <br>
+    <p>Made by <font color="grey">Wood</font></p>
+    <br>
+    <h1><font color="blue">VERSION: latest</h1>
+  </div>
+</div>
+</body>
+</html>
